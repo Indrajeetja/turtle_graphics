4e3cad53c166d84fdec4d03c62aae803b9502d76
Indrajeet Jadhav <indrajeet.jadhav@abzooba.com>
2023-08-04 13:13:49 +0530

    push

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+timy_the_turtle = Turtle()
+timy_the_turtle.shape("turtle")
+timy_the_turtle.color("red")
+
+timy_the_turtle.pendown()
+timy_the_turtle.forward(5)
+screen = Screen()
+screen.exitonclick()
+
